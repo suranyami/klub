77c34f324ed5bb7ea4949c5042fc4cad969391ef
David Parry <david.parry@suranyami.com>
2018-11-05 18:06:09 +1100

    Add missing comma

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,7 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  SERVER_DOMAIN: '"localhost:4000"',
+  SERVER_SCHEME: '"http"'
 })
