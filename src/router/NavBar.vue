3815023eb7e2b2290b9dfaa115a26ccf17c3d56f
David Parry <david.parry@fairfaxmedia.com.au>
2018-12-11 18:10:03 +1100

    Extract NavBar into own vue component

--- src/router/NavBar.vue
+++ src/router/NavBar.vue
@@ -1,11 +1,36 @@
 <template>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="https://bulma.io">
+        <h1 class="title">Klub</h1>
+      </a>
+
+      <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start">
+        <router-link to="/" class="navbar-item">Home</router-link>
+        <router-link to="/page2" class="navbar-item">Page2</router-link>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
 export default {
-  name: 'NavBar'
+  name: 'NavBar',
+  data () {
+    return {
+      title: 'Something'
+    }
+  }
 }
 </script>
 
 <style>
+
 </style>
