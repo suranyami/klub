b1ae6f224d707b6d8b963ec662502247ca42be3e
David Parry <david.parry@fairfaxmedia.com.au>
2018-12-12 18:17:37 +1100

    Create skeleton login field

--- src/auth/Login.vue
+++ src/auth/Login.vue
@@ -1,5 +1,35 @@
 <template>
-  <div>
+  <div class="container has-text-centered">
+    <div class="column is-4 is-offset-4">
+      <h3 class="title">Login</h3>
+      <p class="subtitle">Please login to proceed.</p>
+      <div class="box">
+        <form>
+          <div class="field">
+            <div class="control">
+              <input class="input is-large" type="email" placeholder="Your Email" autofocus="">
+            </div>
+          </div>
+
+          <div class="field">
+            <div class="control">
+              <input class="input is-large" type="password" placeholder="Your Password">
+            </div>
+          </div>
+          <div class="field">
+            <label class="checkbox">
+              <input type="checkbox">
+              Remember me
+            </label>
+          </div>
+          <button class="button is-block is-info is-large is-fullwidth">Login</button>
+        </form>
+      </div>
+      <div class="tabs has-text-centered">
+        <router-link to="/register" class="navbar-item">Register</router-link>
+        <router-link to="/forgot" class="navbar-item">Forgot Password</router-link>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,5 +39,15 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+  @import '@/assets/main.scss';
+
+  .input {
+    border-color: $grey-lighter;
+  }
+  .login-helpers {
+    .navbar-item {
+      width: 40%;
+    }
+  }
 </style>
