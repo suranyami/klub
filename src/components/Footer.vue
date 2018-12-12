562acd102b0f2297ca17661a7d78dc3e5810d97d
David Parry <david.parry@fairfaxmedia.com.au>
2018-12-12 18:18:34 +1100

    Create footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav class="tabs is-boxed is-fullwidth">
+    <div class="container">
+      <ul>
+        <li><a>About</a></li>
+        <li><a>Terms &amp; Conditions</a></li>
+        <li><a>Contact Us</a></li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style scoped>
+</style>
