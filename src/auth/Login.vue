08afa29cd7e366eeeeb4ed3442c43c6136e755d3
David Parry <david.parry@fairfaxmedia.com.au>
2019-01-07 17:58:45 +1100

    Refactor to use buefy fields and vee-validate validations

--- src/auth/Login.vue
+++ src/auth/Login.vue
@@ -1,87 +1,85 @@
 <template>
-  <div class="container has-text-centered">
+  <section>
     <div class="column is-6 is-offset-2">
       <h3 class="title">Login</h3>
       <p class="subtitle">Please login to proceed.</p>
       <div class="box">
-        <form>
-          <div class="field">
-            <div class="control">
-              <input
-                v-show="!login"
+        <form @submit.prevent="validateBeforeSubmit">
+          <template v-if="!login">
+            <b-field label="First name"
+              :type="{'is-danger': errors.has('firstName')}"
+              :message="errors.first('firstName')">
+              <b-input
                 v-model="firstName"
-                class="input"
-                type="text"
-                placeholder="First name">
-            </div>
-          </div>
+                name="firstName"
+                v-validate="'required'"
+                key="firstName-input" />
+            </b-field>
 
-          <div class="field">
-            <div class="control">
-              <input
-                v-show="!login"
-                v-model="lastName"
-                class="input"
-                type="text"
-                placeholder="Last name">
-            </div>
-          </div>
+            <b-field label="Last name"
+              :type="{'is-danger': errors.has('lastName')}"
+              :message="errors.first('lastName')">
+                <b-input
+                  v-model="lastName"
+                  name="lastName"
+                  v-validate="'required'"
+                  key="lastName-input" />
+            </b-field>
+          </template>
 
-          <div class="field">
-            <div class="control">
-              <input
-                v-model="email"
-                class="input"
-                type="email"
-                placeholder="Your Email"
-                autofocus="">
-            </div>
-          </div>
+          <b-field label="Email"
+            :type="{'is-danger': errors.has('email')}"
+            :message="errors.first('email')">
+            <b-input
+              v-model="email"
+              name="email"
+              v-validate="'required|email'"
+              data-vv-as="email"
+              placeholder="Your Email"
+              autofocus=""
+              key="email-input"/>
+          </b-field>
 
-          <div class="field">
-            <div class="control">
-              <input
-                v-model="password"
-                class="input"
-                type="password"
-                placeholder="Your Password">
-            </div>
-          </div>
+          <b-field label="Password"
+            :type="{'is-danger': errors.has('password')}"
+            :message="errors.first('password')">
+            <b-input
+              type="password"
+              v-model="password"
+              name="password"
+              v-validate="'required|min:8'"
+              key="password-input"/>
+          </b-field>
 
-          <!-- <div class="field">
-            <label class="checkbox">
-              <input type="checkbox"/> Remember me
-            </label>
-          </div>
- -->
-          <div class="field is-grouped">
-            <div class="control">
-              <button
-                type="submit"
-                class="button is-block is-info"
-                @click="confirm()">
-                {{login ? 'Login' : 'Create account'}}
-              </button>
-            </div>
-            <div class="control">
-              <button
-                type="submit"
-                class="button is-text"
-                @click="login = !login">
-                {{login ? 'Sign Up' : 'Already have an account?'}}
-              </button>
-            </div>
-
-          </div>
+          <b-field>
+            <button
+              type="submit"
+              class="button is-block is-info"
+              @click="confirm()">
+              {{login ? 'Login' : 'Create account'}}
+            </button>
+            <button
+              type="submit"
+              class="button is-text"
+              @click="login = !login">
+              {{login ? 'Sign Up' : 'Already have an account?'}}
+            </button>
+        </b-field>
         </form>
       </div>
     </div>
-  </div>
+  </section>
 </template>
 
 <script>
 import { MY_USER_ID, AUTH_TOKEN } from '../constants/settings'
 import { CREATE_USER_MUTATION, LOGIN_USER_MUTATION } from './mutations'
+import Vue from 'vue'
+import VeeValidate from 'vee-validate'
+
+Vue.use(VeeValidate, {
+  events: 'input|blur'
+})
 
 export default {
   name: 'Login',
@@ -95,6 +93,18 @@ export default {
     }
   },
   methods: {
+    validateBeforeSubmit () {
+      this.$validator.validateAll().then((result) => {
+        if (result) {
+          return
+        }
+        this.$toast.open({
+          message: 'Form is not valid! Please check the fields.',
+          type: 'is-danger',
+          position: 'is-bottom'
+        })
+      })
+    },
     confirm () {
       const { firstName, lastName, email, password } = this.$data
       if (this.login) {
@@ -105,8 +115,11 @@ export default {
           const id = result.data.login.user.id
           const token = result.data.login.token
           this.saveUserData(id, token)
+          this.$router.push({ path: '/users' })
         }).catch((error) => {
+          console.log(error)
           alert(error)
+          this.$router.push({ path: '/login' })
         })
       } else {
         this.$apollo.mutate({
@@ -118,10 +131,16 @@ export default {
             password
           }
         }).then((result) => {
-          const id = result.data.createUser.user.id
-          const token = result.data.createUser.token
-          this.saveUserData(id, token)
+          if (result.errors) {
+            alert(result.errors)
+            this.$router.push({ path: '/login' })
+          } else {
+            const id = result.data.createUser.user.id
+            const token = result.data.createUser.token
+            this.saveUserData(id, token)
+          }
         }).catch((error) => {
+          console.log(error)
           alert(error)
           this.$router.push({ path: '/login' })
         })
